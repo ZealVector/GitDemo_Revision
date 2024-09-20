f70f32f77ea882122ae577cc379f11331e7c2b30
namarata23sharma <69240468+namarata23sharma@users.noreply.github.com>
2024-09-20 19:26:06 +0530

    Second Commit

--- src/pages/RegisterPage.java
+++ src/pages/RegisterPage.java
@@ -1,14 +1,18 @@
 package pages;
 
-public class LoginPage
-{
-	public static void main(String args[])
+public class RegisterPage {
+
+	public static void main(String[] args)
 	{
+		// TODO Auto-generated method stub
 		String username="admin";
 		String password="admin";
 
 		System.out.println("This is Login Page");
 		System.out.println("This is for third commit");
 		
+		
+		
 	}
+
 }
---
+++ src/tests/RegisterTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class RegisterTest {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		String username="admin";
+		String password="admin";
+
+		System.out.println("This is Login Page");
+		
+	}
+
+}
